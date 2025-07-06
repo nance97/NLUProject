5be9b904d8e8dd79556c17e08b7c7063623f9038
nance97 <nancy.kalaj@studenti.unipd.it>
2025-07-06 16:12:39 +0200

    small fix

--- 257021_Nancy_Kalaj/LM/part_A/model.py
+++ 257021_Nancy_Kalaj/LM/part_A/model.py
@@ -3,11 +3,9 @@ import torch.nn as nn
 class LM_RNN(nn.Module):
     def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1, emb_dropout=0.1, n_layers=1):
         super(LM_RNN, self).__init__()
-        # Token ids to vectors, we will better see this in the next lab 
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
         self.rnn = nn.RNN(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
         self.pad_token = pad_index
-        # Linear layer to project the hidden layer to our output space 
         self.output = nn.Linear(hidden_size, output_size)
         
     def forward(self, input_sequence):
